bfc024c4dda2eb0a943ea55f9be263f101a0c9d9
Lennert Mertens <lennert.mertens@gmail.com>
2024-12-03 23:16:34 +0100

    Update image

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,11 @@
+import VersaMindsLogo from '@/images/versaminds.png'
+import Image from 'next/image'
 export function Logo({className, type}) {
   // Type can be white to use white logo
   // const image = type === 'white' ? "/codecloud-white.svg" : "/codecloud.svg"
-  const image = "/versaminds.png"
   return (
     <div className={className}>
-      <img src={image} alt="VersaMinds logo" className="w-full" />
+      <Image src={VersaMindsLogo} alt="VersaMinds logo" className="w-full" />
     </div>
   )
 }
